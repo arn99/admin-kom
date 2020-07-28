a7076e1a0a50012ea607498079ea8cff77ff2f3b
rams99 <aroldrams@gmail.com>
2020-07-28 13:58:45 +0000

    text error fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,22 +11,17 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { OrderService } from './services/order.service';
 import { FoodService } from './services/food.service';
-import {NgxPaginationModule} from 'ngx-pagination';
-import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { firebaseConfig } from './config/firebase-config';
-import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
-import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
 import { AuthService } from './services/auth.service';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { PwaService } from './services/pwa.service';
-import { PromptComponent } from './components/prompt-component/prompt-component.component';
 
 const initializer = (pwaService: PwaService) => () => pwaService.initPwaPrompt();
 
@@ -37,8 +32,6 @@ const initializer = (pwaService: PwaService) => () => pwaService.initPwaPrompt()
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgxPaginationModule,
-    Ng2SearchPipeModule,
     HttpClientModule,
     BrowserAnimationsModule,
     HeaderModule,
